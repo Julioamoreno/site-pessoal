2a5c4597f98f9e4f00f0f32830feb5b1c9147a09
Júlio Armando <julioarmando321@gmail.com>
2020-03-04 19:07:49 -0300

    :speech_balloon: Alterando informações do contato

--- src/componentes/contato.js
+++ src/componentes/contato.js
@@ -12,14 +12,11 @@ const Contato = props => {
                 <div className='row'>
                     <div className='col-sm-12'>
                         <p><span className='glyphicon glyphicon-envelope'> </span>  julioarmando321@gmail.com</p>
-                        <div className='row'>
-                            <div className='col-sm-5'>
-                                <p><span className='glyphicon glyphicon-phone'> </span>  21 97153-9360</p>
-                            </div>
-                            <div className='col-sm-5'>
-                                <p><img src={IconWhatsapp} alt='whatsapp' width='20' height='20'/> 21 99350-1692 </p>
-                            </div>
-                        </div>
+                        
+                                
+                        <p><img src={IconWhatsapp} alt='whatsapp' width='20' height='20'/> 21 97153-9360</p>
+                            
+                        
                         <p> <a href='http://www.linkedin.com/in/julioamoreno' className="text-decoration-none"> <img src={IconLinkedin} alt='linkedin' width='20' height='20'/> linkedin.com/a/julioamoreno </a>  </p>
                         <p> <a href='http://www.github.com/julioamoreno' className="text-decoration-none"> <img src={IconGithub} alt='github' width='20' height='20'/>   github.com/julioamoreno </a>  </p>
                     </div>
