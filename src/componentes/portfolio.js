403b3dbacda1b5378827d7834f57dd55e27b5d8a
Júlio Armando <julioarmando321@gmail.com>
2020-09-18 19:46:34 -0300

    Add novos projetos ao portfolio

--- src/componentes/portfolio.js
+++ src/componentes/portfolio.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import GIFComoGastei from '../utils/image/ComoGastei.gif';
+import GIFCalculadoraSalario from '../utils/image/CalculadoraSalario.gif';
 import IMGMailMarketing from '../utils/image/Login-cortado.png';
 import IMGSemanaOmni11 from '../utils/image/Semana-Omni_stack11-cortado.png';
 import IMGSemanaOmni10 from '../utils/image/Semana-Omni_stack10-cortado.png';
 
-const Portfolio = (props) => {
+const Portfolio = props => {
 	return (
 		<div className="bg-light">
 			<div className="container-fluid">
@@ -18,6 +20,74 @@ const Portfolio = (props) => {
 					<div className="col-sm-6 col-lg-4">
 						<div className="card border-secondary mb-4 box-shadow">
 							<div className="card-header px-0">
+								<h3>Como Gastei</h3>
+							</div>
+							<img
+								className="card-img-top img-thumbnail"
+								alt="Gif desmonstração site como gastei"
+								src={GIFComoGastei}
+							/>
+							<div className="card-body proj-desc py-2 py-sm-0 py-md-2">
+								<p className="card-text">
+									WebApp para ajudar nas finanças pessoais, onde o
+									usuário pode cadastrar as despesas/receitas, sendo
+									possível restringir a lista a partir de uma barra de
+									buscas. Tecnologias: React, NodeJS, MongoDB.
+								</p>
+							</div>
+							<div className="card-footer proj-links px-4">
+								<a
+									href="https://github.com/Julioamoreno/ControleFinanceiro-node-react"
+									className="card-link"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Github
+								</a>
+								<a
+									href="http://comogastei.herokuapp.com/"
+									className="card-link"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Site
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-6 col-lg-4">
+						<div className="card border-secondary mb-4 box-shadow">
+							<div className="card-header px-0">
+								<h3>Calculadora de Salário Liquido</h3>
+							</div>
+							<img
+								className="card-img-top img-thumbnail"
+								alt="Gif desmonstração site como gastei"
+								src={GIFCalculadoraSalario}
+							/>
+							<div className="card-body proj-desc py-2 py-sm-0 py-md-2">
+								<p className="card-text">
+									Webapp que discrimina todos os descontos de um
+									salário e informa a porcentagem de cada fatia do
+									salário bruto, com informação escrita e também
+									através de um componente visual. Tecnologia: React
+								</p>
+							</div>
+							<div className="card-footer proj-links px-4">
+								<a
+									href="https://github.com/Julioamoreno/calculadora-salario-liquido-com-React"
+									className="card-link"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Github
+								</a>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-6 col-lg-4">
+						<div className="card border-secondary mb-4 box-shadow">
+							<div className="card-header px-0">
 								<h3>Mail Marketing</h3>
 							</div>
 							<img
@@ -27,8 +97,9 @@ const Portfolio = (props) => {
 							/>
 							<div className="card-body proj-desc py-2 py-sm-0 py-md-2">
 								<p className="card-text">
-									Projeto de disparos de email, através da API mailgun, com uso
-									das tecnologias: React, MongoDB, NodeJS, Express.
+									Projeto de disparos de email, através da API
+									mailgun, com uso das tecnologias: React, MongoDB,
+									NodeJS, Express.
 								</p>
 							</div>
 							<div className="card-footer proj-links px-4">
@@ -63,10 +134,10 @@ const Portfolio = (props) => {
 							/>
 							<div className="card-body proj-desc py-2 py-sm-0 py-md-2">
 								<p className="card-text">
-									Projeto do curso Omnistack10 feito pela RocketSeat, que
-									consiste em criar uma aplicação para pessoas encontrarem
-									desenvolvedores Uso das tecnologias: React, NodeJS, Express,
-									Sqlite e Knex.JS
+									Projeto do curso Omnistack10 feito pela RocketSeat,
+									que consiste em criar uma aplicação para pessoas
+									encontrarem desenvolvedores Uso das tecnologias:
+									React, NodeJS, Express, Sqlite e Knex.JS
 								</p>
 							</div>
 							<div className="card-footer proj-links px-4">
@@ -93,7 +164,8 @@ const Portfolio = (props) => {
 							/>
 							<div className="card-body proj-desc py-2 py-sm-0 py-md-2">
 								<p className="card-text">
-									Uso das tecnologias: React,NodeJS, Express, Sqlite e Knex.JS
+									Uso das tecnologias: React,NodeJS, Express, Sqlite e
+									Knex.JS
 								</p>
 							</div>
 							<div className="card-footer proj-links px-4">
