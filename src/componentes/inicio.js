ca4dbbaa0f1c8880ce4d9164e6556771caab2322
julioamoreno <julioarmando321@gmail.com>
2021-02-17 17:10:01 -0300

    feat: alterando sobre da pagina inicial

--- src/componentes/inicio.js
+++ src/componentes/inicio.js
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Inicio = (props) => {
 	return (
-		<div className="container-fluid">
-			<div className="row" id="topo">
-				<div className="col text-center my-auto">
+		<div className='container-fluid'>
+			<div className='row' id='topo'>
+				<div className='col text-center my-auto'>
 					<h1>Júlio Armando</h1>
 					<h2>Programador Web Júnior</h2>
 				</div>
 			</div>
 
-			<div className="row" id="sobre">
-				<div className="col-sm-8 my-auto">
+			<div className='row' id='sobre'>
+				<div className='col-sm-8 my-auto'>
 					<h2>Sobre Júlio Armando</h2>
 					<p>
-						Uma pessoa focada, analítica, amante de novas tecnologias. Sempre em
-						busca de conhecimentos que possibilitem evoluir minhas áreas de
-						conhecimento, tentando sempre estar atento aos novos
-						direcionamentos.
+						Sempre em busca de conhecimentos que possibilitem evoluir minhas
+						áreas de conhecimento, tentando sempre estar atento aos novos
+						direcionamentos do mercado. Atualmente trabalhando como freelancer,
+						fullstack Javascript (React, Node.JS, Express e Adonis).
 					</p>
 				</div>
 			</div>
